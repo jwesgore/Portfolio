dd0226c0b2949a0bc4d459fbd4e8af51ee96b7ed
jwesgore <65801327+jwesgore@users.noreply.github.com>
2020-10-30 02:54:07 -0400

    Software Engineering Project
    Design a semi-working visual prototype of a program used to help reduce unfairness in the workplace workloads

---
+++ Task Manager 11/Employee/inbox.js
@@ -0,0 +1,20 @@
+function message_mexican() {
+        window.location.href='./message_mexican.html';
+}
+
+function message_hey() {
+        window.location.href='./message_hey.html';
+}
+
+function message_wallet() {
+        window.location.href='./message_wallet.html';
+}
+
+function create() {
+	window.location.href = './create.html';
+}
+
+function send() {
+	alert("Message sent");
+	window.location.href="./inbox.html"
+}
---
+++ Task Manager 11/Manager/inbox.js
@@ -0,0 +1,28 @@
+function message_sarah() {
+	window.location.href='./message_sarah.html';
+}
+
+function message_alex() {
+        window.location.href='./message_alex.html';
+}
+
+function message_almamy() {
+        window.location.href='./message_almamy.html';
+}
+
+function message_wes() {
+        window.location.href='./message_wes.html';
+}
+
+function message_heath() {
+        window.location.href='./message_heath.html';
+}
+
+function create() {
+	window.location.href = './create.html';
+}
+
+function send() {
+	alert("Message sent");
+	window.location.href="./inbox.html"
+}
